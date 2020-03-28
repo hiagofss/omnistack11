b778f193f8b43a03713fea70d58924aa0f497ceb
Hiago Souza <hiagofss98@gmail.com>
2020-03-28 13:15:12 -0300

    Mobile project OmniStack 11

---
+++ mobile/src/routes.js
@@ -0,0 +1,24 @@
+// In App.js in a new project
+
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import Incidents from './pages/Incidents';
+import Detail from './pages/Detail';
+
+const AppStack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <AppStack.Navigator screenOptions={{ headerShown: false }}>
+        <AppStack.Screen name='Incidents' component={Incidents} />
+        <AppStack.Screen name='Detail' component={Detail} />
+      </AppStack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
